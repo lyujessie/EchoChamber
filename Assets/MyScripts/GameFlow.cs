0bea773b01b7a1ea34574df5421fa09e24ce9a4b
Jessie Lyu <jiexinlyu@Jessies-MacBook-Pro-2.local>
2019-11-11 18:01:22 -0800

    added timer

---
+++ Assets/MyScripts/GameFlow.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using TMPro;
+
+public class GameFlow : MonoBehaviour
+{
+    public float timeLeft = 60.0f; //set each level to be 30 seconds
+    private TMP_Text Timer;
+
+    // Use this for initialization
+    void Start()
+    {
+        Timer = GetComponent<TMP_Text>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timeLeft -= Time.deltaTime;
+        Timer.text = timeLeft.ToString("0");
+    }
+}
